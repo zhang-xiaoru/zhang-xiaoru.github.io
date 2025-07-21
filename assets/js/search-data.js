d555b313fa8a60a552129191b916c818af1b0fd5
zhang-xiaoru <93516795+zhang-xiaoru@users.noreply.github.com>
2025-07-21 02:24:19 +0000

    Deploying to gh-pages from @ zhang-xiaoru/zhang-xiaoru.github.io@861d6499ec76670012c6f287c181eb98f74be62c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-research",
           title: "research",
-          description: "Research interest",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/research/";
